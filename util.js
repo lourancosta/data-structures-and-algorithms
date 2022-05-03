e7cb6a7e5d38f958ddeea0156b41f44fb5e22688
Louran Costa <louran.costa@yahoo.com.br>
2022-05-03 00:41:17 -0300

    Add function defaultToString

--- util.js
+++ util.js
@@ -1,3 +1,19 @@
 export function defaultEquals(a, b) {
     return a === b
 }
+
+export function defaulToString(item) {
+    if (item === null) {
+        return 'NULL'
+    }
+
+    if (item === undefined) {
+        return 'UNDEFINED'
+    }
+
+    if (typeof item === 'string' || item instanceof String) {
+        return `${item}`
+    }
+
+    return item.toString()
+}
