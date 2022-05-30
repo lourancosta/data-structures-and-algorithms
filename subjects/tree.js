6484a3c571ea819b9592d6870d35ecde1f076f93
Louran Costa <louran.costa@yahoo.com.br>
2022-05-30 10:28:31 -0300

    Added new methodos

--- subjects/tree.js
+++ subjects/tree.js
@@ -356,5 +356,167 @@ class RedBlackTree extends BinarySearchTree {
         this.compareFn = compareFn
         this.root = null
     }
-    
+
+    insert(key: T) {
+        if (this.root == null) {
+            this.root = new RedBlackNode(key)
+            this.root.color = Colors.BLACK
+        } else {
+            const newNode = this. insertNode(this.root, key)
+            this.fixTreeProperties(newNode)
+        }
+    }
+
+    insertNode(node, key) {
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            if (node.left == null) {
+                node.left = new RedBlackNode(key)
+                node.left.parent = node
+                return node.left
+            } else {
+                return this.insertNode(node.left, key)
+            }
+        } else if (node.right == null) {
+            module.right = mode.right = new rebb
+        }
+    }
+}
+
+class RedBlackNode extends Node {
+    constructor(key) {
+        super(key)
+        this.key = key
+        this.color = Colors.RED
+        this.parent = null
+    }
+
+    isRed() {
+        return this.color === Colors.RED
+    }
+
+    insertNode(node, key) {
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            if (node.left == null) {
+                node.left = new RedBlackNode(key)
+                node.left.parent = node
+                return node.left
+            } else {
+                return this.insertNode(node.left, key)
+            }
+        } else if (node.right == null) {
+            node.right = new RedBlackNode(key)
+            node.right.parent = node
+            return node.right
+        } else {
+            return this.insertNode(node.right, key)
+        }
+    }
+
+    fixTreeProperties(node) {
+        while (node && node.parent && node.parent.color.isRed() && node.color !== Colors.BLACK) {
+            let parent = node.parent
+            const grandParent = parent.parent
+
+            // caso A: o pai é o filho a esquerda
+            if (grandParent && grandParent.left === parent) {
+                const uncle = grandParent.right
+                
+                // caso A1: o tio do nó tambem é vermelho - basta recolorir
+                if (uncle && uncle.color === Colors.Red) {
+                    grandParent.color = Colors.RED
+                    parent.color = Colors.BLACK
+                    uncle.color = Colors.BLACK
+                    node = grandParent
+                } else {
+                    // caso A2: o nó é o filho a direita - rotacao a esquerda
+                    if (node === parent.right) {
+                        this.rotationRR(parent)
+                        node = parent
+                        parent = node.parent
+                    }
+
+                    // caso A3: o nó é o filho a esquerda - rotacao a direita
+                    this.rotationLL(grandParent)
+                    parent.color = Colors.BLACK
+                    grandParent.color = Colors.RED
+                    node = parent
+                }
+            } else { // caso B: o pai é o filho a direita
+                const uncle = grandParent.left
+
+                // caso B1: o tio é vermelho - basta recolorir
+                if (uncle && uncle.color === Colors.RED) {
+                    grandParent.color = Colors.RED
+                    parent.color = Colors.BLACK
+                    uncle.color = Colors.BLACK
+                    node = grandParent
+                } else {
+                    // caso B2: o nó é o filho a esquerda – rotação a direita
+                    if (node === parent.left) {
+                        this.rotationLL(parent)
+                        node = parent
+                        parent = node.parent
+                    }
+
+                    // caso B3: o nó é o filho a direita – rotação a esquerda
+                    this.rotationRR(grandParent)
+                    parent.color = Colors.BLACK
+                    grandParent.color = Colors.RED
+                    node = parent
+                }
+            }
+        }
+
+
+
+        this.root.color = Colors.BLACK
+    }
+
+    rotationLL(node) {
+        const tmp = node.left
+        node.left = tmp.right
+
+        if (tmp.right && tmp.right.key) {
+            tmp.right.parent = node
+        }
+
+        tmp.parent = node.parent
+
+        if (!node.parent) {
+            this.root = tmp
+        } else {
+            if (node === node.parent.left) {
+                node.parent.left = tmp
+            } else {
+                node.parent.right = tmp
+            }
+        }
+
+        tmp.right = node
+        node.parent = tmp
+    }
+
+    rotationRR(node) {
+        const tmp = node.right
+        node.right = tmp.left
+
+        if (tmp.left && tmp.left.key) {
+            tmp.left.parent = node
+        }
+
+        tmp.parent = node.parent
+
+        if (!node.parent) {
+            this.root = tmp
+        } else {
+            if (node === node.parent.left) {
+                node.parent.left = tmp
+            } else {
+                node.parent.right = tmp
+            }
+        }
+
+        tmp.left = node
+        node.parent = tmp
+    }
 }
