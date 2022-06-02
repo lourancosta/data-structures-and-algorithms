84bf38c364443c4576147f913995ce8ddefbe5c9
Louran Costa <louran.costa@yahoo.com.br>
2022-06-01 23:56:00 -0300

    Update heap

--- subjects/heap.js
+++ subjects/heap.js
@@ -61,8 +61,8 @@ export class MinHeap {
 const heap = new MinHeap()
 heap.insert(2)
 heap.insert(3)
-heap.insert(4)
-heap.insert(5)
+// heap.insert(4)
+// heap.insert(5)
 heap.insert(1)
 
 console.log(heap)
