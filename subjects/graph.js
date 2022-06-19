94544d0e42244f1e1289437f53e27a4a43ffeed1
Louran Costa <louran.costa@yahoo.com.br>
2022-06-19 14:48:01 -0300

    Added Prim and Kruskal algorithms

--- subjects/graph.js
+++ subjects/graph.js
@@ -315,7 +315,7 @@ var graphDijkstra = [
     [0, 0, 0, 0, 0, 0]
 ]
 
-const INF = Number.MAX_SAFE_INTEGER
+//const INF = Number.MAX_SAFE_INTEGER
 const dijkstra = (graphDijkstra, src) => {
     const dist = []
     const visited = []
@@ -395,4 +395,114 @@ const floydWarshall = graph => {
 
     return dist
 }
-console.log(floydWarshall(graphFW))
+//console.log(floydWarshall(graphFW))
+
+
+// ----------------------------------  MST - Arvore de extensao minima ----------------------------------
+// ----------------------------------  ALGORITMO DE PRIM ----------------------------------
+const graphPrim = [
+    [0, 2, 4, 0, 0, 0],
+    [2, 0, 2, 4, 2, 0],
+    [4, 2, 0, 0, 3, 0],
+    [0, 4, 0, 0, 3, 2],
+    [0, 2, 3, 3, 0, 2],
+    [0, 0, 0, 2, 2, 0]
+]
+
+const INF = Number.MAX_SAFE_INTEGER
+const prim = graph => {
+    const parent = []
+    const key = []
+    const visited = []
+    const { length } = graph
+
+    for (let i = 0; i < length; i++) {
+        key[i] = INF
+        visited[i] = false
+    }
+
+    key[0] = 0
+    parent[0] = -1
+    
+    for (let i = 0; i < length - 1; i++) {
+        const u = minKey(graph, key, visited)
+        visited[u] = true
+        
+        for (let v = 0; v < length; v++) {
+            if (graph[u][v] && !visited[v] && graph[u][v] < key[v]) {
+                parent[v] = u
+                key[v] = graph[u][v]
+            }
+        }
+    }
+
+    return parent
+}
+
+const minKey = (graph, key, visited) => {
+    let min = INF
+    let minIndex = -1
+    const { length } = graph
+
+    for (let v = 0; v < length; v++) {
+        if (visited[v] === false && key[v] <= min) {
+            min = key[v]
+            minIndex = v
+        }
+    }
+
+    return minIndex
+}
+console.table(prim(graphPrim))
+
+
+// ----------------------------------  ALGORITMO DE KRUSKAL ----------------------------------
+const kruskal = graph => {
+    const { length } = graph
+    const parent = []
+    let ne = 0
+    let a 
+    let b
+    let u
+    let v
+    const cost = initializeCost(graph)
+
+    while (ne < length - 1) {
+        for (let i = 0, min = INF; i < length; i++) {
+            for (let j = 0; j < length; j++) {
+                if (cost[i][j] < min) {
+                    min = cost[i][j]
+                    a = u = i
+                    b = v = j
+                }
+            }
+        }
+
+        u = find(u, parent)
+        v = find(v, parent)
+
+        if (union(u, v, parent)) {
+            ne++
+        }
+
+        cost[a][b] = cost[b][a] = INF
+    }
+
+    return parent
+}
+
+const find = (i, parent) => {
+    while (parent[i]) {
+        i = parent[i]
+    }
+
+    return i
+}
+
+const union = (i, j, parent) => {
+    if (i !== j) {
+        parent[j] = i
+        return true
+    }
+    return false
+}
